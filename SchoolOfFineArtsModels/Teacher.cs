6c31e46bf2780c02394ae1c238e5d5bb33d8de96
Erik Tabaka <eriktabaka@outlook.com>
2022-10-25 09:23:48 -0500

    Added teachers table and added SQL interaction

--- SchoolOfFineArtsModels/Teacher.cs
+++ SchoolOfFineArtsModels/Teacher.cs
@@ -1,16 +1,21 @@
-﻿namespace SchoolOfFineArtsModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolOfFineArtsModels
 {
     public class Teacher
     {
+        [Key]
         public int Id { get; set; }
+        [Required, StringLength(50)]
         public string FirstName { get; set; }
+        [StringLength(50)]
         public string LastName { get; set; }
+        [Required, Range(1, 130)]
         public int Age { get; set; }
+        
 
         //override equals to take in an object and compare to teacher
-
-
-        
+  
         public override string ToString()
         {
             return $"First Name: {FirstName}, Last Name: {LastName}. ID:{Id}, Age: {Age}";
